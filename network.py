6e5cfede1d91283bce8e08ac086bb00de400f7d3
Angel Lacret <alacret@gmail.com>
2022-10-06 22:34:24 -0400

    Change URL to change connection

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import urllib
 
 def check_network_conn():
     try:
-        urllib.request.urlopen('https://google.com')
+        urllib.request.urlopen('https://csfcouriersltd.com')
         return True
     except:
         return False
