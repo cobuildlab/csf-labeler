dee309cb840ee503b4d1bba83fd508d8235a0b03
Angel Lacret <alacret@gmail.com>
2022-10-10 09:26:14 -0400

    clean jobs antes de mandar a imprimir

--- printer.py
+++ printer.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 conn = cups.Connection()
 
+
 def __get_printer_info():
     printers = conn.getPrinters()
     for printer_config in PRINTERS_CONFIG:
@@ -13,21 +14,28 @@ def __get_printer_info():
             state = printers[printer_name]['printer-state']
             serial = device_uri.split('=')[1]
             return printer_name, device_uri, state, serial
-    return "", "","",""
+    return "", "", "", ""
 
-def get_printer_serial()-> Optional[str]:
+
+def get_printer_serial() -> Optional[str]:
     return __get_printer_info()[3]
 
-def is_printer_ready()-> Optional[bool]:
+
+def is_printer_ready() -> Optional[bool]:
     printer_status = __get_printer_info()[2]
     if printer_status == 3:
         return True
     else:
         return False
 
+
 def send_to_printer(filename):
-    try: 
+    try:
+        conn.cancelAllJobs()
+    except Exception as e:
+        print("printer.py:send_to_printer:cancelAllJobs")
+
+    try:
         conn.printFile(__get_printer_info()[0], filename, "Printing " + filename, {})
     except ValueError as e:
         print("It seems that the printer is OFF", e)
-
