e8372960e89be9cfbc57e69c96a35af12a4e5523
Angel Lacret <alacret@gmail.com>
2022-10-16 18:30:54 -0400

    remove .vevn

--- etc/setup.sh
+++ etc/setup.sh
@@ -18,10 +18,6 @@ sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get update
 sudo apt-get install -y python3-kivy
 
-# VENV
-python3 -m venv venv
-source venv/bin/activate
-
 pip3 install -r requirements.txt
 
 #  conf files
--- start.sh
+++ start.sh
@@ -7,9 +7,6 @@ pwd
 git reset --hard
 git pull origin main
 #gnome-terminal --window-with-profile=csf -- python3 /home/cobuild/csf-labeler/app.py -f
-bash etc/setup.sh
-python3 -m venv venvs
-source venv/bin/activate
 pip3 install -r requirements.txt
 pyclean -v .
 python3 app.py
