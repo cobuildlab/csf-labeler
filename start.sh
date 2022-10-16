23a9d81e48707c026ef688354839d5ad0968eb03
Angel Lacret <alacret@gmail.com>
2022-10-16 18:17:49 -0400

    deps

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ pwd
 git reset --hard
 git pull origin main
 #gnome-terminal --window-with-profile=csf -- python3 /home/cobuild/csf-labeler/app.py -f
+bash etc/setup.sh
 python3 -m venv venvs
 source venv/bin/activate
 pip3 install -r requirements.txt
