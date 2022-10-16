607f1cf2d69aefb1ffbcade7197ded4d4d72f192
Angel Lacret <alacret@gmail.com>
2022-10-16 17:13:56 -0400

    pulling from main

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ sleep 5
 cd /home/cobuild/csf-labeler
 pwd
 git reset --hard
-git pull origin
+git pull origin main
 #gnome-terminal --window-with-profile=csf -- python3 /home/cobuild/csf-labeler/app.py -f
+python3 -m venv venvs
 source venv/bin/activate
 pip3 install -r requirements.txt
 pyclean -v .
