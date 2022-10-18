e433d7861534369b66c009f232a8ff74e5d3336a
Angel Lacret <alacret@gmail.com>
2022-10-17 23:06:16 -0400

    sqlite can't be on multi threading

--- csf_db.py
+++ csf_db.py
@@ -1,32 +1,27 @@
 import sqlite3
 from threading import Thread
 
-con = sqlite3.connect('csf.db')
+connections = sqlite3.connect('csf.db')
 
 
-class DBSaver(Thread):
-    def __init__(self, count, day_lot):
-        Thread.__init__(self, name="DBSaver")
-        self.count = count
-        self.day_lot = day_lot
+def _save_count_and_day_lot(count, day_lot):
+    global connections
+    update_sql = 'UPDATE SETTING SET value = ? WHERE key = ?'
+    update_data = [
+        (count, 'count'),
+        (day_lot, 'day_lot')
+    ]
 
-    def run(self):
-        update_sql = 'UPDATE SETTING SET value = ? WHERE key = ?'
-        update_data = [
-            (self.count, 'count'),
-            (self.day_lot, 'day_lot')
-        ]
-
-        with con:
-            con.executemany(update_sql, update_data)
+    with connections:
+        connections.executemany(update_sql, update_data)
 
 
 def fetch_count_and_day_lot():
-    global con
+    global connections
     day_lot = 0
     count = 0
-    with con:
-        settings_data = con.execute("SELECT * FROM SETTING")
+    with connections:
+        settings_data = connections.execute("SELECT * FROM SETTING")
         for row in settings_data:
             if row[0] == 'day_lot':
                 day_lot = int(row[1])
@@ -36,17 +31,17 @@ def fetch_count_and_day_lot():
 
 
 def save_count_and_day_lot(count, day_lot):
-    DBSaver(count, day_lot).start()
+    _save_count_and_day_lot(count, day_lot)
 
 
 def reset_count_and_day_lot(count=0, day_lot=0):
-    DBSaver(count, day_lot).run()
+    _save_count_and_day_lot(count, day_lot)
 
 
-with con:
+with connections:
     # Create Structure
     try:
-        con.execute("""
+        connections.execute("""
             CREATE TABLE SETTING (
                 key TEXT NOT NULL PRIMARY KEY,
                 value TEXT
@@ -62,6 +57,6 @@ with con:
         ('day_lot', '0')
     ]
     try:
-        con.executemany(sql, data)
+        connections.executemany(sql, data)
     except sqlite3.IntegrityError:
         print("csf_db.py:Key already exists:")
--- tests/db_tests.py
+++ tests/db_tests.py
@@ -1,12 +1,11 @@
 import unittest
-from csf_db import DBSaver, fetch_count_and_day_lot, reset_count_and_day_lot
-from csf_network import RequestSender
+from csf_db import fetch_count_and_day_lot, reset_count_and_day_lot, save_count_and_day_lot
 
 
 class DBTestCase(unittest.TestCase):
     def test_save(self):
         day_lot, count = fetch_count_and_day_lot()
-        DBSaver(day_lot + 1, count + 1).run()
+        save_count_and_day_lot(day_lot + 1, count + 1)
         updated_day_lot, updated_count = fetch_count_and_day_lot()
         self.assertEqual(day_lot + 1, updated_day_lot)
         self.assertEqual(count + 1, updated_count)
